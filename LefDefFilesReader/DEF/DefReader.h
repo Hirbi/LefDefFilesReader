9d8a221b342dab381f97a5c0bdc533c60aade83c
Hirbi <roma.sh.14@mail.ru>
2023-06-01 19:03:12 +0300

    init Design, DefReader classes

---
+++ LefDefFilesReader/DEF/DefReader.h
@@ -0,0 +1,7 @@
+#pragma once
+
+#include "Design/Design.h"
+
+class DefReader {
+	
+};
--- LefDefFilesReader/main.cpp
+++ LefDefFilesReader/main.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
+#include "DEF/DefReader.h"
 using namespace std;
 
 
 int main() {
+	DefReader defReader
 	cout << "Hello world";
 }
