8c4c0f71d0f81d86494e567a7d522c1fb479bb1f
Hirbi <roma.sh.14@mail.ru>
2023-07-14 22:21:15 +0300

    add Placement to Port

--- LefDefFilesReader/LEF/Macro/Pin/Port/Port.cpp
+++ LefDefFilesReader/LEF/Macro/Pin/Port/Port.cpp
@@ -1,6 +1,19 @@
 #include "Port.h"
 
+using namespace std;
+
 void Port::addLayer(Layer layer)
 {
     this->layers.insert({ layer.getName(), layer });
+}
+
+void Port::SetPlacement(const string& type, const Point& pt, const string& orient) {
+    Placement.Type = type;
+    Placement.Pt = pt;
+    Placement.Orient = orient;
+}
+
+PlaceParams Port::GetPlacement() const
+{
+    return PlaceParams();
 }
--- LefDefFilesReader/LEF/Macro/Pin/Port/Port.h
+++ LefDefFilesReader/LEF/Macro/Pin/Port/Port.h
@@ -8,7 +8,11 @@ class Port
 {
 private:
     std::unordered_map<std::string, Layer> layers;
+	PlaceParams Placement;
 public:
     void addLayer(Layer layer);
+
+	void SetPlacement(const string& type, const Point& pt, const string& orient);
+	PlaceParams GetPlacement() const;
 };
 
